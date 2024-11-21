d569b7965d4ce55e7338e3f12531e8594bda10c5
already-love <756843722@qq.com>
2024-11-21 20:13:39 +0000

    inheritance templates

--- mypage/mypage/settings.py
+++ mypage/mypage/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # BASE_DIR/"challenges"/"templates"   #这个路径必须叫做templates。复数
+            BASE_DIR/"templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
