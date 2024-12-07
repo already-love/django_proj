087c2b2d0793a305dfe7fffd788560edd16a48c4
already-love <756843722@qq.com>
2024-12-07 20:47:30 +0000

    Relationships complete

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,15 +4,45 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=80) 
+    code = models.CharField(max_length=2)
+
+    def __str__(self):    #此处必须写self
+        return f"{self.name}"
+    class Meta:    #nested class
+        verbose_name_plural = "Countries"   # 默认是Address s
+
+class Address(models.Model):
+    street = models.CharField(max_length=80) 
+    postal_code = models.CharField(max_length=5) 
+    city = models.CharField(max_length=50) 
+    
+    def __str__(self):    #此处必须写self
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:    #nested class
+        verbose_name_plural = "Address Entries"   # 默认是Address s
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100) 
     last_name = models.CharField(max_length=100) 
+    address = models.OneToOneField(Address,on_delete=models.CASCADE, null= True)  # related_name默认author
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):    #此处必须写self
+        return self.full_name()
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     # author = models.CharField(null=True, max_length=100)  #可以为null默认值
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)  # CASCADE意味着一个author删除，相应书同样删除
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, 
+                                null=True, related_name="books")  # CASCADE意味着一个author删除，相应书同样删除
+    published_countries = models.ManyToManyField(Country, related_name="books")    #不加on_delete
+
     is_bestselling = models.BooleanField(default=True)
     slug = models.SlugField(default="", blank=True, 
                             null=False, db_index= True)  # 如果用这个query，可以设置为index。但是设置太多column为index会降低效率。
