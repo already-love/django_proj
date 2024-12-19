b8f6c6f0e953e155d8a5084bf3891c8c32a42704
already-love <756843722@qq.com>
2024-12-19 17:14:57 +0000

    use FileField to deal with image upload

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    image= models.FileField(upload_to="images")  #在settings.py 的MEDIA_ROOT
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -3,16 +3,29 @@ from django.views import View
 from django.http import HttpResponseRedirect
 # Create your views here.
 
-def store_file(file):
-    with open(f"temp/{file}", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+from .forms import ProfileForm
+from .models import UserProfile
+
+# def store_file(file):
+#     with open(f"temp/{file}", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html",{
+            "form":form,              
+        })
 
     def post(self, request):
-        store_file(request.FILES['image'])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            # store_file(request.FILES['image'])
+            profile = UserProfile(image= request.FILES["user_image"])   #forms.py里面的
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+        return render(request, "profiles/create_profile.html",{
+            "form":submitted_form,              
+        })
