2b85df5522be1d823e38e5a3103b6e25acbfaa2c
already-love <756843722@qq.com>
2024-12-19 17:37:48 +0000

    ImageField CreateView

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-    image= models.FileField(upload_to="images")  #在settings.py 的MEDIA_ROOT
+    image= models.ImageField(upload_to="images")  #在settings.py 的MEDIA_ROOT
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 from .forms import ProfileForm
@@ -11,21 +12,8 @@ from .models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
-
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html",{
-            "form":form,              
-        })
-
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        if submitted_form.is_valid():
-            # store_file(request.FILES['image'])
-            profile = UserProfile(image= request.FILES["user_image"])   #forms.py里面的
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-        return render(request, "profiles/create_profile.html",{
-            "form":submitted_form,              
-        })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
